bb0afe1ae74efba5a93cc1b0e5f624c7cf8c6978
Angelo Frigeri Araujo <angelo.araujo@pixeon.com>
2021-04-08 11:17:06 -0300

    added db file and django models

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from ecommerce.models import (
+    Brand,
+    Product,
+    Sell,
+    ProductEntry,
+    ProductExit,
+)
+
+
+class ProductExitInlines(admin.TabularInline):
+    model = ProductExit
+    extra = 0
+
+
+class SellAdmin(admin.ModelAdmin):
+    inlines = [ProductExitInlines]
+
+
+admin.site.register(Brand)
+admin.site.register(Product)
+admin.site.register(Sell, SellAdmin)
+admin.site.register(ProductEntry)
+admin.site.register(ProductExit)
--- ecommerce/models.py
+++ ecommerce/models.py
@@ -1,3 +1,71 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.timezone import now as datetime_now
+
+
+class Brand(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100, unique=False, verbose_name="Nome")
+
+    class Meta:
+        verbose_name = "Marca"
+        verbose_name_plural = "Marcas"
+
+    def __str__(self):
+        return f"{self.name} - {str(self.id)}"
+
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100, unique=True, verbose_name="Nome")
+    description = models.TextField(verbose_name="Descrição")
+    price = models.FloatField(verbose_name="Preço")
+    brand = models.ForeignKey(to=Brand, on_delete=models.CASCADE, verbose_name="Marca")
+    image = models.ImageField(verbose_name="Imagem")
+
+    class Meta:
+        verbose_name = "Produto"
+        verbose_name_plural = "Produtos"
+
+    def __str__(self):
+        return f"{self.name} - {self.brand.name}"
+
+
+class Sell(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    date = models.DateField(default=datetime_now, verbose_name="Data")
+
+    class Meta:
+        verbose_name = "Venda"
+        verbose_name_plural = "Vendas"
+
+    def __str__(self):
+        return f"{self.date} - {self.id}"
+
+
+class ProductExit(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE, verbose_name="Produto")
+    quantity = models.IntegerField(verbose_name="Quantidade")
+    sell = models.ForeignKey(to=Sell, on_delete=models.CASCADE, verbose_name="Venda")
+
+    class Meta:
+        verbose_name = "Item de Venda"
+        verbose_name_plural = "Itens de Venda"
+
+    def __str__(self):
+        return f"{self.sell.date} - {self.product.name}:{self.quantity} - {self.product.brand.name}"
+
+
+class ProductEntry(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE, verbose_name="Produto")
+    quantity = models.IntegerField(verbose_name="Quantidade")
+
+    class Meta:
+        verbose_name = "Entrada de Produto"
+        verbose_name_plural = "Entradas de Produtos"
+
+    def __str__(self):
+        return f"{self.product.name}:{self.quantity} - {self.product.brand.name}"
